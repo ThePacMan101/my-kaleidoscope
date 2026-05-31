f85fd3584b82b25bc388b15151d83594ae64a05b
Lucas Pimentel <lulalucas.27@gmail.com>
2026-05-31 02:22:22 -0300

    feat: further parser improvements\adds identifier parsing\ adds primary parsing\adds operator precedence

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,11 +1,24 @@
 #include "parser.hpp"
 #include "lexer.hpp"
 #include "ast.hpp"
+#include <array>
 #include <iostream>
 
 namespace parser{
 
 static int curr_tok;
+consteval std::array<int,256> build_precedence_data(){
+    std::array<int,256> table{};
+    for(int& val: table) val = -1;
+
+    table['<'] = 10;
+    table['+'] = 20;
+    table['-'] = 20;
+    table['*'] = 40;
+    
+    return table;
+}
+constexpr std::array<int,256> binary_operator_precedence = build_precedence_data();
 
 static int get_next_token(){
     return curr_tok = lexer::get_tok();
@@ -21,7 +34,22 @@ static std::unique_ptr<ast::Prototype> log_prototype_error(const char* str){
     return nullptr;
 }
 
-static std::unique_ptr<ast::Expr> number_expr(){
+static int get_tok_precedence(){
+    if(!isascii(curr_tok))
+        return -1;
+
+    int token_precedence = binary_operator_precedence[curr_tok];
+    if(token_precedence <= 0) return -1;
+    return token_precedence;
+}
+
+
+static std::unique_ptr<ast::Expr> expression(){
+    // TODO
+    return nullptr;
+}
+
+static std::unique_ptr<ast::Expr> number(){
     // curr token is a number literal
     auto result = std::make_unique<ast::NumberExpr>(
         lexer::get_num_val()
@@ -30,7 +58,7 @@ static std::unique_ptr<ast::Expr> number_expr(){
     return std::move(result);
 }
 
-static std::unique_ptr<ast::Expr> paren_expr(){
+static std::unique_ptr<ast::Expr> paren(){
     // curr token is a '('
     get_next_token(); // skip '('
     auto expr = expression();
@@ -48,20 +76,38 @@ static std::unique_ptr<ast::Expr> identifier(){
         // when I parse some identifier, if I don't call it, I presume it's just a variable
         return std::make_unique<ast::VariableExpr>(identifier_name);
     get_next_token(); // skip '('
-    ast::Expr* args;
-    
+    std::vector<std::unique_ptr<ast::Expr>> args;
+    if(curr_tok != ')'){
+        while(true){
+            if(auto arg = expression())
+                args.push_back(std::move(arg));
+            else
+                return nullptr;
+            
+            if(curr_tok == ')') break;
 
+            if(curr_tok != ',') return log_error("Expected ')' or ',' in argument list.");
+            get_next_token();
+        }
+    }   
+    get_next_token(); // skip ')'
 
+    return std::make_unique<ast::CallExpr>(identifier_name, std::move(args));
 }
 
-static std::unique_ptr<ast::Expr> expression(){
-    // switch (curr_tok){
-    //     case lexer::tok_eof:
-    //     case lexer::tok_def:
-    //     case lexer::tok_extern:
-    //     case lexer::tok_identifier:
-    //     case lexer::tok_number:
-    // }
+static std::unique_ptr<ast::Expr> primary(){
+    switch (curr_tok){
+        default: 
+            return log_error("Unknown token when expecting an expression.");
+        case lexer::tok_identifier:
+            return identifier();    
+        case lexer::tok_number:
+            return number();    
+        case '(':
+            return paren();    
+    }
 }
+
+
 
 }
