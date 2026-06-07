5365c7f75385a2f50dfa6520ea29334637d5c7e4
Lucas Pimentel <lulalucas.27@gmail.com>
2026-06-07 18:25:55 -0300

    feat: adds Nullable concept to error logger to assert returntype's correctness.

--- include/logger.hpp
+++ include/logger.hpp
@@ -4,8 +4,12 @@
 
 namespace logger {
 
-/* Must have nullable return type */
-template <typename returntype> 
+namespace detail {
+    template <typename T>    
+    concept Nullable = std::is_constructible_v<T,std::nullptr_t>;
+}
+
+template <detail::Nullable returntype> 
 returntype error(const char* str){
     std::cerr << "[ERROR]: " << str << ".\n";
     return nullptr;
