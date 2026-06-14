5b51eb75ed0049b9a7ca4d1c23970680d5e5ef99
Lucas Pimentel <lulalucas.27@gmail.com>
2026-06-14 00:47:02 -0300

    feature: implements the Optimizer constructor

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -1,8 +1,27 @@
 #include <llvm/Passes/PassBuilder.h>
 #include <llvm/Passes/StandardInstrumentations.h>
+#include <memory>
 
 #include "optimizer.hpp"
 
 namespace opt{
 
+Optimizer::Optimizer(llvm::LLVMContext& context){
+    function_pass_manager = std::make_unique<llvm::FunctionPassManager>();
+    loop_analysis_manager = std::make_unique<llvm::LoopAnalysisManager>();
+    function_analysis_manager = std::make_unique<llvm::FunctionAnalysisManager>();
+    CGSCC_analysis_manager = std::make_unique<llvm::CGSCCAnalysisManager>();
+    module_analysis_manager = std::make_unique<llvm::ModuleAnalysisManager>();
+
+    pass_instrumentation_callbacks = std::make_unique<llvm::PassInstrumentationCallbacks>();
+    standard_instrumentations = std::make_unique<llvm::StandardInstrumentations>(context, 
+        true /*debugging log*/
+    );
+
+    standard_instrumentations->registerCallbacks(
+        *pass_instrumentation_callbacks,
+        module_analysis_manager.get()
+    );
+
+}
 }
