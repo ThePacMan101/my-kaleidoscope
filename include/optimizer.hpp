179b1e9f37539d81604069b29e4264076949a61b
Lucas Pimentel <lulalucas.27@gmail.com>
2026-06-17 22:24:30 -0300

    docs: removes exccess documentation polluting Optimizer

--- include/optimizer.hpp
+++ include/optimizer.hpp
@@ -14,21 +14,6 @@ class Optimizer{
 
         struct Impl;
         std::unique_ptr<Impl> pimpl;
-        /*
-        std::unique_ptr<llvm::ModuleAnalysisManager> module_analysis_manager;
-        std::unique_ptr<llvm::CGSCCAnalysisManager> CGSCC_analysis_manager; // (C)all (G)raph (S)trongly (C)onnected (C)omponent
-        std::unique_ptr<llvm::FunctionAnalysisManager> function_analysis_manager;
-        std::unique_ptr<llvm::LoopAnalysisManager> loop_analysis_manager;
-        
-        // We only run passes in functions
-
-        std::unique_ptr<llvm::PassBuilder> pass_builder;
-        std::unique_ptr<llvm::FunctionPassManager> function_pass_manager;
-
-        std::unique_ptr<llvm::PassInstrumentationCallbacks> pass_instrumentation_callbacks;
-        std::unique_ptr<llvm::StandardInstrumentations> standard_instrumentations;
-        */
-
 
     public:
         Optimizer(llvm::LLVMContext& context);
